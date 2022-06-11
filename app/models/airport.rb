39a63d070b3a60a482c4f0286310ed080887d1cf
Ben <robinsonbena@gmail.com>
2022-06-11 10:23:08 -0700

    Associations
    Airport model was not able to call arriving_flights or departing_flights methods because the associations were not defined in the model. Add these custom names to model. Tested, and methods now work on Airport

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,5 +1,5 @@
 class Airport < ApplicationRecord
 
-  has_many :flights, foreign_key: :dept_airport
-  has_many :flights, foreign_key: :arr_airport
+  has_many :departing_flights, class_name: 'Flight', foreign_key: :dept_airport
+  has_many :arriving_flights, class_name: 'Flight', foreign_key: :arr_airport
 end
