1ee47b4f1ab21ba7b5fd0cfd394cab90ed9dcfe8
Ben <robinsonbena@gmail.com>
2022-06-13 09:05:07 -0700

    Form 1
    Add collection_select to From and To airport dropdowns. They create correct params hash.

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,9 +2,7 @@ class FlightsController < ApplicationController
 
   def index
     @flights = Flight.all
-    @dept_airport_options = @flights.map { |flight| flight.dept_airport.name }.uniq
-    @arr_airport_options = @flights.map { |flight| flight.arr_airport.name }.uniq
-    @date_options = @flights.map { |flight| flight.date_time }.uniq
+    @airport_names = Airport.order(:name)
 
 # show flights that meet params ie after form submitted else blank
   end
