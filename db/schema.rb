8dfa4442833fef864a09d7416af3a5c4de08e85a
Ben <robinsonbena@gmail.com>
2022-06-14 11:40:06 -0700

    Passengers
    Remove Address column from passengers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_13_164759) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_14_182345) do
   create_table "airports", force: :cascade do |t|
     t.string "name"
     t.string "symbol"
@@ -44,7 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_13_164759) do
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.string "email"
-    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "unique_emails", unique: true
