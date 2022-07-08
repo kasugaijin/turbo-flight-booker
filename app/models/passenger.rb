9b54cf9d9e9c987a51bc87f0bb39725473258dcb
Ben <robinsonbena@gmail.com>
2022-07-08 10:57:56 -0700

    add required:true to passenger form, name field to ensure name is present before submitting the form

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,6 +3,6 @@ class Passenger < ApplicationRecord
   has_many :bookings, inverse_of: :passengers
   has_many :flights, through: :bookings, inverse_of: :passengers
 
-  validates :name, presence: true, length: { minimum: 5 }
+  validates :name, presence: true
   validates :email, presence: true
 end
