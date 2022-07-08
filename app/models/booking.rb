faece82b486a92b7257916f198e754819d8fad44
Ben <robinsonbena@gmail.com>
2022-07-08 10:25:48 -0700

    Remove validation from Booking model for :passengers because it was preventing the passenger validation from working and you could make a booking without passenger details present.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,6 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :flight
   has_many :passengers, dependent: :destroy
 
-  accepts_nested_attributes_for :passengers, 
-    reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :passengers
 end
